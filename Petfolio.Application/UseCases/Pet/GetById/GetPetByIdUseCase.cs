ca7f94b902c8602343fe71020ad9037e8695457f
Nicolas Freitas <nicolasandrade.de.freitas@gmail.com>
2024-06-23 10:58:38 -0300

    Add project files.

---
+++ Petfolio.Application/UseCases/Pet/GetById/GetPetByIdUseCase.cs
@@ -0,0 +1,19 @@
+﻿using Petfolio.Communication.Responses;
+
+namespace Petfolio.Application.UseCases.Pet.GetById;
+
+public class GetPetByIdUseCase
+{
+    public ResponsePetJson Execute(int id)
+    {
+        return new ResponsePetJson 
+        { 
+            Id = id ,
+            Name = "Teste", 
+            Type = Communication.Enums.PetType.Dog,
+            Birthday = new DateTime(year: 2018,month: 02, day: 27)
+            
+        };
+
+    }
+}
